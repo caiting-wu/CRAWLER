f6412c46323d9a77971ebd29733880be864ee143
caiting-wu <116707838+caiting-wu@users.noreply.github.com>
2023-01-18 20:57:10 +0800

    Update web-crawler.py

--- web-crawler.py
+++ web-crawler.py
@@ -1,21 +1,4 @@
 #https://youtu.be/9Z9xKWfNo7k
-import urllib.request as req
-url="https://www.ptt.cc/bbs/movie/index.html"
-#附加 Headers(不被拒絕)
-request=req.Request(url,headers={
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
-})
-with req.urlopen(request) as response:
-    data=response.read().decode("utf-8")
-
-#解析
-import bs4 
-root=bs4.BeautifulSoup(data,"html.parser")
-titles=root.find_all("div",class_="title")
-for title in titles:
-    if title.a !=None:
-        print(title.a.string)
-
 #cookie(網站在使用者瀏覽器中存放的資料，連線時送出)https://youtu.be/BEA7F9ExiPY
 import urllib.request as req
 def getData(url):
